e245adf3583a4ff6c9d712f3c8e105722adcd9d8
Karuppaiah Alagu <akaruppaiah@CEIIND.COM>
2026-02-12 12:49:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3003;
+
+app.get("/", (req, res) => {
+    res.send("Hello Freshers! This is running from a server.");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
